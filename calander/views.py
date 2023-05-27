8e03143ed2bf338c8518d27afac9ae9f3a56caff
sudhanshu-k <mscientist12@gmail.com>
2023-05-28 01:14:31 +0530

    ready for replit

--- calander/views.py
+++ calander/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.shortcuts import redirect
 from django.http import JsonResponse
 import os
+from google_auth_oauthlib import flow as _flow
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
@@ -13,10 +14,30 @@ SERVER_URL = "http://127.0.0.1:8000"
 # local host
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+# Get the environment variables
+client_id = os.environ.get("client_id")
+client_secret = os.environ.get("client_secret")
+redirect_uri = os.environ.get("redirect_uri")
+
 def GoogleCalendarInitView(request):
     if(request.method=="GET"):
         # oauth2
-        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file("oauth-client.json", scopes=SCOPE_OAUTH)
+        flow = _flow.Flow.from_client_config(
+            client_config={
+                "web": {
+                    "client_id": client_id,
+                    "project_id": "convin-oauth",
+                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                    "token_uri": "https://accounts.google.com/o/oauth2/token",
+                    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                    "client_secret": client_secret,
+                    "redirect_uris": [redirect_uri],
+                    "javascript_origins": ["http://127.0.0.1:8000"],
+                }
+            },
+            scopes=SCOPE_OAUTH
+        )
+
         flow.redirect_uri=SERVER_URL+reverse("getEvents")
         authorization_url, state = flow.authorization_url()
         request.session["state"]=state
